54fa77da343bd9dcaa5ccad11774dd474eb9cbc6
Katharine McClenaghan <123762040+kemcclen@users.noreply.github.com>
2023-08-08 20:16:35 -0400

    Revert "Feature/prototype"

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,27 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Switch,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./component/login";
 import Signup from "./component/signup";
 import {
   ApolloProvider,
   ApolloClient,
   InMemoryCache,
-  HttpLink,
+  createHttpLink,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
-import Searchbar from "./components/Searchbar";
-import SavedPlaylists from "./components/SavedPlaylists";
-import AppNavbar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Searchbar from "./components/Searchbar";
+import AppNavbar from "./components/Navbar";
+import SavedPlaylists from "./components/SavedPlaylists";
 
-const httpLink = new HttpLink({
+const httpLink = createHttpLink({
   uri: "/graphql",
+  credentials: "include",
 });
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem("id_token");
+
   return {
     headers: {
       ...headers,
@@ -44,13 +41,15 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <AppNavbar />
-        <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route index={true} path='/' element={<Searchbar />} />
-          <Route path='/playlists' element={<SavedPlaylists />} />
-        </Routes>
+        <div className='App'>
+          <AppNavbar />
+          <Routes>
+            <Route index={true} path='/' element={<Searchbar />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<Signup />} />
+            <Route path='/playlists' element={<SavedPlaylists />} />
+          </Routes>
+        </div>
       </Router>
     </ApolloProvider>
   );
--- client/src/component/signup.js
+++ client/src/component/signup.js
@@ -4,16 +4,14 @@ import { useNavigate } from "react-router-dom";
 import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../utils/mutations";
 
-const Signup = () => {
+export const Signup = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState(null);
   const navigate = useNavigate();
 
-  const [addUser, { data }] = useMutation(ADD_USER, {
-    variables: { username, password },
-  });
+  const [addUser] = useMutation(ADD_USER);
 
   const handleSignup = async (e) => {
     e.preventDefault();
@@ -24,51 +22,57 @@ const Signup = () => {
     }
 
     try {
-      addUser();
-      console.log(data);
-      navigate("/");
+      await addUser({
+        variables: {
+          username,
+          password,
+        },
+        onCompleted: (data) => {
+          console.log("Signup Successful!", data.addUser);
+          localStorage.setItem("id_token", data.addUser.token);
+          navigate("/login");
+        },
+      });
     } catch (err) {
       setError(err.response.data.message);
     }
   };
 
   return (
-    <>
-      <form className='signup' onSubmit={handleSignup}>
-        <div className='signup__field'>
-          <i className='signup__icon fas fa-user'></i>
-          <input
-            type='text'
-            className='signup__input'
-            placeholder='User name / Email'
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div className='signup__field'>
-          <i className='signup__icon fas fa-lock'></i>
-          <input
-            type='password'
-            className='signup__input'
-            placeholder='Password'
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div className='signup__field'>
-          <i className='signup__icon fas fa-lock'></i>
-          <input
-            type='password'
-            className='signup__input'
-            placeholder='Confirm Password'
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-        </div>
-        {error && <div className='error'>{error}</div>}
-        <button className='button signup__submit'>
-          <span className='button__text'>Sign Up Now</span>
-          <i className='button__icon fas fa-chevron-right'></i>
-        </button>
-      </form>
-    </>
+    <form className='signup' onSubmit={handleSignup}>
+      <div className='signup__field'>
+        <i className='signup__icon fas fa-user'></i>
+        <input
+          type='text'
+          className='signup__input'
+          placeholder='User name / Email'
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div className='signup__field'>
+        <i className='signup__icon fas fa-lock'></i>
+        <input
+          type='password'
+          className='signup__input'
+          placeholder='Password'
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className='signup__field'>
+        <i className='signup__icon fas fa-lock'></i>
+        <input
+          type='password'
+          className='signup__input'
+          placeholder='Confirm Password'
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+      </div>
+      {error && <div className='error'>{error}</div>}
+      <button className='button signup__submit'>
+        <span className='button__text'>Sign Up Now</span>
+        <i className='button__icon fas fa-chevron-right'></i>
+      </button>
+    </form>
   );
 };
 
